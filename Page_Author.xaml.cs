fc9ba8f9a18d6ad80813b89fba6b72553337e5ae
Даниил <dragon0lordchik@gmail.com>
2022-03-31 09:58:53 +0300

    Add project files.

---
+++ Page_Author.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShopPraktika
+{
+    /// <summary>
+    /// Interaction logic for Page_Author.xaml
+    /// </summary>
+    public partial class Page_Author : Page
+    {
+
+        int count = 0;
+        public static ObservableCollection<User> Users { get; set; }
+        public Page_Author()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_event(object sender, RoutedEventArgs e)
+        {
+            Users = new ObservableCollection<User>(bd_connection.connection.User.ToList());
+            var log = Users.Where(a => a.Login.ToString() == txt_login.Text && a.Password.ToString() == txt_password.Password).FirstOrDefault();
+
+            if (log != null)
+            {
+                //сделать window
+            }
+            else
+            {
+                MessageBox.Show("Invalid user", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                ++count;
+            }
+
+            if (count % 3 == 0)
+            {
+                MessageBox.Show("Вы вводили неправильные данные больше 3 раз, блокировка 1 минута", "Блокировка");
+                //заблокать кнопку на минуту
+            }
+        }
+
+        private void Sign_event(object sender, RoutedEventArgs e)
+        {
+
+            NavigationService.Navigate(new Page_Register());
+        }
+
+        private void rdo_button_Checked(object sender, RoutedEventArgs e)
+        {
+            //что то с радио баттоном сделать
+        }
+    }
+}
---
+++ Page_Register.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShopPraktika
+{
+    /// <summary>
+    /// Interaction logic for Page_Register.xaml
+    /// </summary>
+    public partial class Page_Register : Page
+    {
+        public static ObservableCollection<User> Users { get; set; }
+        public Page_Register()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_event(object sender, RoutedEventArgs e)
+        {
+            Regex regex = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[^a-zA-Z0-9])\S{6,16}$");
+
+            MatchCollection matches = regex.Matches(txt_password.ToString());
+
+            if (matches.Count > 1)
+            {
+                try
+                {
+                    User a = new User
+                    {
+                        Login = txt_login.Text,
+                        Password = txt_password.ToString()
+                    };
+                    bd_connection.connection.User.Add(a);
+                    bd_connection.connection.SaveChanges();
+                    MessageBox.Show("OK");
+                    NavigationService.GoBack();
+                }
+                catch (Exception)
+                {
+                    throw new Exception("Error");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Придумай пароль получше");
+            }
+            
+        }
+
+        private void Cancel_event(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}

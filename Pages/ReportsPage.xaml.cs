3310aa0585805244afcb1148cf1dad5ddc5ff991
Даниил <dragon0lordchik@gmail.com>
2022-04-12 10:44:00 +0300

    Сделай что бы датагрид красивым был, потом продолжу

--- Pages/ReportsPage.xaml.cs
+++ Pages/ReportsPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ShopPraktika.Data_;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,14 @@ namespace ShopPraktika
     /// </summary>
     public partial class ReportsPage : Page
     {
+        public List<ProductIntake> ProductIntakes { get; set; }
         public ReportsPage()
         {
             InitializeComponent();
+            
+            ProductIntakes = MainWindow.db.ProductIntake.ToList();
+
+            this.DataContext = this;
         }
     }
 }

97fc6f15c545dbe61ecc2bbdf05f7e0d7a9c36ca
Даниил <dragon0lordchik@gmail.com>
2022-04-16 15:38:26 +0300

    кр4 что то добавил и похуй

--- Pages/IntakesPage.xaml.cs
+++ Pages/IntakesPage.xaml.cs
@@ -36,5 +36,10 @@ namespace ShopPraktika
         {
             NavigationService.GoBack();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new IntakeProductsPage());
+        }
     }
 }
